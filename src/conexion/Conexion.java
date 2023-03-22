aebfa0e3c27d20b38078f41844169014df849d68
alejandrorecarte <alejandro.recarte.rebollo@gmail.com>
2023-03-22 01:20:55 +0100

    22/03/2023 > Main > Conexion > Items > Menus > Usuarios TODO: Crear Usuario

---
+++ src/conexion/Conexion.java
@@ -0,0 +1,44 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.*;
+
+public class Conexion {
+
+    private static Connection connection;
+
+    public Conexion (String usuario, String contrasenya) throws Exception{
+        System.out.println("Conectando con la base de datos");
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/lorazepam",usuario,contrasenya);
+        System.out.println("Conexión con la base de datos establecida");
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
+
+    public static void close() throws SQLException {
+        connection.close();
+    }
+
+    public static String hashPassword(String password) {
+        String hashedPassword = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hashedBytes = md.digest(password.getBytes());
+            hashedPassword = bytesToHex(hashedBytes);
+        } catch (NoSuchAlgorithmException e) {
+            // Manejar la excepción aquí
+        }
+        return hashedPassword;
+    }
+
+    private static String bytesToHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+}
---
+++ src/usuario/Usuario.java
@@ -0,0 +1,14 @@
+package items.usuario;
+
+import java.sql.*;
+
+public class Usuario {
+
+    private String nombre;
+    private String email;
+    private int telefono;
+
+    public void crearUsuario(String nombreUsuario, String hashedContrasenya, String email, int telefono){
+        Statement statement = ;
+    }
+}
